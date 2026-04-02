3e3c11ad7a974dcf92fdf6abbfe123ae59248be9
Matti Lassila <matti.lassila@gmail.com>
2026-04-02 09:55:19 +0300

    Add new instructions to metadata form

--- elements/metadata.php
+++ elements/metadata.php
@@ -15,7 +15,7 @@
  */
 ?>
 <div class="row top-buffer">
-	<div class="col-sm-9 text-info"><?php echo htmlspecialchars($messages['pdfaMetadataMessage'], ENT_QUOTES, 'UTF-8') ?></div>
+	<div class="col-sm text-info"><?php echo htmlspecialchars($messages['pdfaMetadataMessage'], ENT_QUOTES, 'UTF-8') ?></div>
 </div>
 <?php foreach ($configs['metadataField'] as $field) { ?>    
     <div class="row top-buffer">
